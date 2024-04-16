f0105290efb2d8ba161bb4558b1f3c7a1588e5d7
Gui-gt <30755guilherme@gmail.com>
2024-04-15 21:01:59 -0300

    mais sobre c#

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public class enemyAI
+{
+    int speed;
+    float health;
+    string enemyName;
+
+    int[] intarray = new int [] {1,2,3,4,5};
+
+    List<int> intList = new List<int>() {1,2,3,4,5};
+
+
+
+    void Movement () {
+
+            foreach (int intNumbers in intarray)
+            {
+                
+            }
+
+
+        intList.Remove(1)
+        if(speed <= 0 && health > 10)
+        {
+
+        }
+        else{
+
+        }
+    }
+    void Dead ()
+    {
+        if(health <= 0){
+
+        }
+        else
+        {
+
+        }
+    }
+}
